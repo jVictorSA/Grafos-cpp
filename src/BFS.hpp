5b608614b2a918495da36c0064a025d9c5128dba
João Victor Araujo <jvsa@ic.ufal.br>
2022-06-29 16:51:10 -0300

    :bug: BFS corrigido

--- src/BFS.hpp
+++ src/BFS.hpp
@@ -6,25 +6,31 @@
 #include <iostream>
 #include <vector>
 
-void BFS(grafo G, int v){
+void BFS(grafo& G, int v){
     int count = 0;
     std::vector<int> prev;
-
+    int W;
     for(int v = 0; v < G.nArest(); v++)
         prev.push_back(-1);
     
-    fila<int*> F; 
-    F.push(G.getFistVertex());
+    fila<int> F; 
+    F.push(v);
     prev[0] = count++;
 
-    while(!F.empty())
-        int* v = F.pop();
+    while(!F.empty()){
+        W = F.pop();
 
-        for(int e = G.first(v); e < G.nArest(); e = G.next(v, e))
+        for(int e = G.first(W); e < G.nArest(); e = G.next(W, e)){      
+            F.show();
+            if(e == G.nVerts()){
+                continue;
+            }
             if(prev[e] == -1){
                 prev[e] = count++;
-                F.push(G.getIndexVertex(e));
+                F.push(e);
             }
+        }
+    }
 
     F.~fila();
 
